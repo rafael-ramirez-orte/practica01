fa6b413abd60f4d921105e3e3d7fda7ac7a1cef9
rafael.ramirez <rafa.rami12.10@gmail.com>
2022-05-23 23:09:25 -0500

    Feat: some functions were created

---
+++ script.js
@@ -0,0 +1,6 @@
+function changeColor(event){
+    document.getElementById("main").style.backgroundColor = event.value
+}
+function changeColorText(event){
+    document.getElementById("main").style.backgroundColor = event.value
+}
